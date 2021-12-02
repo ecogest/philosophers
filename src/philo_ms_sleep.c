c972696456da0b2b045a9fa9b8a33457c51ab5e9
big-shell <>
2021-12-02 16:13:19 +0100

    capt the waiting time after death (when tt_eat or tt_sleep very long)

--- src/philo_ms_sleep.c
+++ src/philo_ms_sleep.c
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:16:03 by mjacq             #+#    #+#             */
-/*   Updated: 2021/12/02 16:04:33 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/12/02 16:09:51 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	philo_ms_sleep(t_philo *philo, uint ms_start, uint ms_duration)
 {
 	uint	current_time;
+	uint	ms_sleep;
 
 	while (!philo_should_stop(philo))
 	{
@@ -22,6 +23,11 @@ void	philo_ms_sleep(t_philo *philo, uint ms_start, uint ms_duration)
 		if (current_time >= (ms_start + ms_duration))
 			break ;
 		else
-			usleep(1000 * ((ms_start + ms_duration) - current_time) / 2);
+		{
+			ms_sleep = ((ms_start + ms_duration) - current_time) / 2;
+			if (ms_sleep > MAX_MS_SLEEP_TIME)
+				ms_sleep = MAX_MS_SLEEP_TIME;
+			usleep(1000 * ms_sleep);
+		}
 	}
 }
